e7dbd940c51dc7fc8d6ea8463ec9360c01db9049
Martín Rabasa <martinrabasa@gmail.com>
2021-08-18 23:00:37 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,70 +1,98 @@
 /* eslint-disable */
 
-import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import SearchBar from './components/SearchBar';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  const [curLocation, setCurrentLocation] = useState({country: "AR", lat: -34.6132, lon: -58.3772, name: "Buenos Aires"});
-  
+  const [curLocation, setCurrentLocation] = useState({
+    country: "AR",
+    lat: -34.6132,
+    lon: -58.3772,
+    name: "Buenos Aires",
+  });
+  const [loading, setLoading] = useState(true);
   const [weatherData, setWeatherData] = useState(null);
-  const [geocodingData, setGeocodingData] = useState([]);
 
-  const clearGeocodingData = () => setGeocodingData([]);
   const newLocation = (data) => setCurrentLocation(data);
 
-  const requestWeather = async () => await axios
-    .get(`/.netlify/functions/request-weather?lat=${curLocation.lat}&lon=${curLocation.lon}`)
-    .then(res => {
-      setWeatherData(res.data);
-      console.log(res.data)
-    });
-
-  const geocoding = async (input) => await axios
-    .get(`/.netlify/functions/geocoding?input=${input}`)
-    .then(res => {
-      setGeocodingData(res.data);
-    })
-
-  const passed = async () => {
-    await navigator.geolocation.getCurrentPosition(pos => axios
-      .get(`/.netlify/functions/reverse-geocoding?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}`)
-      .then(res => setCurrentLocation(res.data[0])));
+  const requestWeather = async () => {
+    setLoading(true);
+    await axios
+      .get(
+        `/.netlify/functions/request-weather?lat=${curLocation.lat}&lon=${curLocation.lon}`
+      )
+      .then((res) => {
+        setWeatherData(res.data);
+        console.log(res.data);
+      });
+    setLoading(false);
   }
 
+  const reverseGeocoding = async () => {
+    await navigator.geolocation.getCurrentPosition((pos) =>
+      axios
+        .get(`/.netlify/functions/reverse-geocoding?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}`)
+        .then((res) => setCurrentLocation(res.data[0]))
+    );
+  };
+
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(passed);
+    navigator.geolocation.getCurrentPosition(reverseGeocoding);
   }, []);
 
   useEffect(() => {
     requestWeather();
-  }, [curLocation])
+  }, [curLocation]);
 
   return (
     <div className="App">
-      <SearchBar geocoding={geocoding} 
-                  geocodingData={geocodingData} 
-                  clearGeocodingData={clearGeocodingData}
-                  newLocation={newLocation}/>
-      <div className="weather">
-        {weatherData && <div className="current-weather">
+      <SearchBar newLocation={newLocation} />
+      {loading && <div className="loading">
+        <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+      </div>}
+      {weatherData && !loading && <div className="weather">
+        <div className="current-weather">
           <h1 className="cur-location">{`${curLocation.name}, ${curLocation.country}`}</h1>
-          <div className="weather-icon"><img src={`http://openweathermap.org/img/w/${weatherData.current.weather[0].icon}.png`} alt={weatherData.current.weather[0].main} title={weatherData.current.weather[0].main} width="70px" height="70px" /></div>
-          <h1 className="temp">{weatherData && Math.round(weatherData.current.temp * 10) / 10 + " ºC"}</h1>
-        </div>}
-        {weatherData && <div className="daily-weather">
-          {weatherData.daily.map((day, index) => <div className="day-weather" key={index}>
-            <div className="date">{`${new Date(new Date().setDate(new Date().getDate() + index)).getDate()} / 
-                ${new Date(new Date().setDate(new Date().getDate() + index)).getMonth()}`}</div>
-            <div className="weather-icon"><img src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt={day.weather[0].main} title={day.weather[0].main}/></div>
-            <div className="daily-minmax">
-              <div className="max">{Math.round(day.temp.max)}</div>
-              <div className="min">{Math.round(day.temp.min)}</div>
+          <div className="weather-icon">
+            <img
+              src={`http://openweathermap.org/img/w/${weatherData.current.weather[0].icon}.png`}
+              alt={weatherData.current.weather[0].main}
+              title={weatherData.current.weather[0].main}
+              width="70px"
+              height="70px"
+            />
+          </div>
+          <h1 className="temp">
+            {weatherData &&
+              Math.round(weatherData.current.temp * 10) / 10 + " ºC"}
+          </h1>
+        </div>
+        <div className="daily-weather">
+          {weatherData.daily.map((day, index) => (
+            <div className="day-weather" key={index}>
+              <div className="date">{`${new Date(
+                new Date().setDate(new Date().getDate() + index)
+              ).getDate()} / 
+              ${new Date(
+                new Date().setDate(new Date().getDate() + index)
+              ).getMonth()}`}</div>
+              <div className="weather-icon">
+                <img
+                  src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`}
+                  alt={day.weather[0].main}
+                  title={day.weather[0].main}
+                />
+              </div>
+              <div className="daily-minmax">
+                <div className="max">{Math.round(day.temp.max)}</div>
+                <div className="min">{Math.round(day.temp.min)}</div>
+              </div>
             </div>
-          </div>)}  
-        </div>}
-      </div>
+          ))}
+        </div>
+      </div>}
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,14 @@
 /* eslint-disable */
 
 import './SearchBar.css';
+import axios from "axios";
 import { useEffect, useState, useRef } from 'react';
 
 const SearchBar = (props) => {
     const [value, setValue] = useState("");
     const [searchResultsClass, setSearchResultsClass] = useState("search-results hidden");
+    const [geocodingData, setGeocodingData] = useState([]);
+    const [loading, setLoading] = useState(false);
  
     const firstRender = useRef(true);
     
@@ -23,6 +26,16 @@ const SearchBar = (props) => {
         }, 150)
     }
     
+    const geocoding = async (input) => {
+        setLoading(true);
+        await axios
+            .get(`/.netlify/functions/geocoding?input=${input}`)
+            .then((res) => {
+                setGeocodingData(res.data);
+            });
+        setLoading(false);
+    }
+
     useEffect(() => {
         if (firstRender.current) {
             firstRender.current = false;
@@ -30,25 +43,34 @@ const SearchBar = (props) => {
         }
 
         const timeoutId = setTimeout(() => {
-            props.clearGeocodingData();
+            setGeocodingData([]);
             if(value !== ''){
-                props.geocoding(value);
+                geocoding(value);
             }
         }, 1000);
         
         return () => clearTimeout(timeoutId);
     }, [value]);
 
+    
+
     return (
         <div className="search-field">
-            <input className="search-input" 
-                onFocus={showSearchResults} 
-                onBlur={hideSearchResults}  
-                type="text" 
-                placeholder="Enter a city..." 
-                onChange={handleInputChange}/>
+            <div className="search-bar">
+                <input className="search-input" 
+                        onFocus={showSearchResults} 
+                        onBlur={hideSearchResults}  
+                        type="text" 
+                        placeholder="Enter a city..." 
+                        onChange={handleInputChange}/>
+                <div className="icon">
+                    {!loading ? 
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="search-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg> :
+                    <div class="lds-ring"><div></div><div></div><div></div><div></div></div>}
+                </div>
+            </div>
             <div className={searchResultsClass}>
-                {props.geocodingData.map((data, index) => 
+                {geocodingData.map((data, index) => 
                     <div key={index} className="search-result" 
                         onClick={() => props.newLocation(data)}>{data.name}, {data.country}</div>)}
             </div>
